40968dfdfbe1eee62ae467a5aef5a8337d6f6eae
Luan de Oliveira <luan012012@hotmail.com>
2024-09-21 17:47:40 -0300

    refactor: alterado main, utilizando método says

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use ferris_says::say;
+use std::io::{
+    stdout,BufWriter
+};
+
+fn main(){
+    let stdout = stdout();
+    let message = String::from("Olá, Dexter Morgan.");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
