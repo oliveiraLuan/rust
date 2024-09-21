06b6216bd0187ddf5d890363d7ccf6cf287b15f7
Luan de Oliveira <luan012012@hotmail.com>
2024-09-20 23:11:48 -0300

    Projeto criado

---
+++ hello-rust/target/debug/deps/hello_rust-1ff7b1f3411fafdb.d
@@ -0,0 +1,5 @@
+/Users/luandeoliveira/Documents/rust-getting-started/hello-rust/target/debug/deps/hello_rust-1ff7b1f3411fafdb: src/main.rs
+
+/Users/luandeoliveira/Documents/rust-getting-started/hello-rust/target/debug/deps/hello_rust-1ff7b1f3411fafdb.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/luandeoliveira/Documents/rust-getting-started/hello-rust/target/debug/hello-rust: /Users/luandeoliveira/Documents/rust-getting-started/hello-rust/src/main.rs
