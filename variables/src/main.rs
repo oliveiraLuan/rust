8c31b1e3ab0aabb72851cec2b90cc094aa03bed2
Luan de Oliveira <luan012012@hotmail.com>
2024-09-23 23:17:43 -0300

    Realizando shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,19 @@ fn main(){
     println!("O valor de x é {x}");
     x = 6;
     println!("O valor de x agora é {x}");
+
+    shadow();
+}
+
+fn shadow(){
+    let x = 1;
+    
+    let x = x + 1;
+
+    println!("X nesse contexto vale {x}");
+
+    {
+        let x = x * 2;
+        println!("X nesse contexto vale: {x}");
+    }
 }
